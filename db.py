f8861be9a857d3a65e91913b7cde12b42687648f
Ziabaa <y1vd196@gmail.com>
2024-04-10 16:54:22 +0300

    modify

--- db.py
+++ db.py
@@ -5,13 +5,28 @@ class DataBase:
     def __init__(self, connect):
         self.connect = connect
 
-    def execute_query_json(self, query):
+    def execute_query_json(self, param):
+
+        if param == "calls":
+            check_group_value = 4
+        elif param == "chats":
+            check_group_value = 3
+        else:
+            return None
+
+        query = """SELECT cl.Code, cl.Promt
+            FROM CheckGroupLists cgl
+            JOIN CheckLists cl ON cgl.CheckList = cl.id
+            WHERE cgl.CheckGroup = {}
+            FOR JSON PATH;""".format(check_group_value)
+
         cursor = self.connect.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
         json_data = json.loads(result[0][0])
         cursor.close()
-        return json_data
+        res = DataBase.formatting_to_json(json_data)
+        return res
 
     @staticmethod
     def formatting_to_json(json_data):
